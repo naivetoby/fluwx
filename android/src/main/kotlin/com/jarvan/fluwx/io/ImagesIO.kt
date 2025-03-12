5f4de9eedc46ae750c3e5cdb9536f9e745735f6e
陶开杰 <naivetoby@gmail.com>
2025-03-12 19:02:31 +0800

    toLowerCase -> lowercase

--- android/src/main/kotlin/com/jarvan/fluwx/io/ImagesIO.kt
+++ android/src/main/kotlin/com/jarvan/fluwx/io/ImagesIO.kt
@@ -102,7 +102,7 @@ class ImagesIOIml(override val image: WeChatFile) : ImagesIO {
 
         val byteArrayOutputStream = ByteArrayOutputStream()
         var format = CompressFormat.PNG
-        if (suffix.toLowerCase(Locale.US) == ".jpg" || suffix.toLowerCase(Locale.US) == ".jpeg") {
+        if (suffix.lowercase() == ".jpg" || suffix.lowercase() == ".jpeg") {
             format = CompressFormat.JPEG
         }
         bitmap.compress(format, 100, byteArrayOutputStream)
